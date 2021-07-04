446d5c98bcd181fa0d9cc0c06ced21981f9ad2c6
spamegg <spamegg@pm.me>
2021-07-04 23:51:04 +0300

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,180 @@
+import io.{BufferedSource}
+import io.Source.fromResource             // this can "see" the resources folder
+import io.StdIn.readLine
+import util.Random.between
+import util.control.Breaks._
+
+type Hand = Map[Char, Int]
+type Words = List[String]
+
+val FILENAME: String = "words.txt"
+val VOWELS: String = "aeiou"
+val CONSONANTS: String = "bcdfghjklmnpqrstvwxyz"
+val HANDSIZE: Int = 10
+val VOWELRATIO: Int = 3
+val BONUS: Int = 50
+val SCRABBLE: Hand = Map(
+  'a' -> 1, 'b' -> 3, 'c' -> 3, 'd' -> 2, 'e' -> 1, 'f' -> 4, 'g' -> 2,
+  'h' -> 4, 'i' -> 1, 'j' -> 8, 'k' -> 5, 'l' -> 1, 'm' -> 3, 'n' -> 1,
+  'o' -> 1, 'p' -> 3, 'q' -> 10, 'r' -> 1, 's' -> 1, 't' -> 1, 'u' -> 1,
+  'v' -> 4, 'w' -> 4, 'x' -> 8, 'y' -> 4, 'z' -> 10
+)
+
+
+/**
+ * @param filename string name of a file in /src/main/resources, like "words.txt"
+ *                 each line in file contains exactly one word.
+ * @return list of words in the file.
+ */
+def loadWords(filename: String): Words =
+  val file: BufferedSource = fromResource(filename)
+  val words: Words = file.getLines.toList
+  file.close
+  println(f"${words.size} words loaded.")
+  words
+
+
+/**
+ * @param word String e.g. "hello"
+ * @return map of char -> int pairs indicating how many times a letter occurs
+ *         e.g. Map('h' -> 1, 'e' -> 1, 'l' -> 2, 'o' -> 1)
+ */
+def getFreqMap(word: String): Hand =
+  word.groupBy(identity).view.mapValues(_.size).toMap
+
+
+/**
+ * @param word string (the word that is played in that hand)
+ * @param num integer (usually HANDSIZE)
+ * @return the score for the word. It is calculated as follows:
+ *         sum of scrabble scores of each letter in word,
+ *         multiplied by the length of the word,
+ *         plus a potential bonus (if max number of letters were used).
+ */
+def getWordScore(word: String, num: Int): Int =
+  val letterScores: Int = (for letter <- word yield SCRABBLE(letter)).sum
+  letterScores * word.size + (if word.size == num then BONUS else 0)
+
+
+/**
+ * @param hand map of char, integer pairs; like 'a' -> 5
+ *             prints each letter as many times as they appear in hand.
+ */
+def displayHand(hand: Hand): Unit =
+  val letters: Iterable[String] =
+    for (letter, count) <- hand yield f"$letter " * count
+  println(f"Current hand: ${letters.mkString}")
+
+
+/**
+ * @param handSize integer, the total number of letters in hand being dealt
+ * @param numVowels integer, the number of vowels in hand being dealt
+ * @return hand: a map of char -> int pairs, showing how many of each letter.
+ */
+def dealHand(handSize: Int, numVowels: Int): Hand =
+  val vowels: Iterable[Char] =
+    for _ <- 1 to numVowels yield VOWELS(between(0, VOWELS.size))
+
+  val consonants: Iterable[Char] =
+    for _ <- numVowels + 1 to handSize
+    yield CONSONANTS(between(0, CONSONANTS.size))
+
+  getFreqMap(vowels.mkString + consonants.mkString)
+
+
+/**
+ * @param hand current hand: map of char -> int pairs
+ * @param word currently guessed word to be removed from hand.
+ *             assume all letters in word (however many times) appear in hand.
+ * @return updated hand with letters of word removed from hand.
+ */
+def updateHand(hand: Hand, word: String): Hand =
+  val handFromWord: Hand = getFreqMap(word)
+  hand.map((letter, count) => handFromWord.get(letter) match
+    case Some(num) => (letter, count - num)
+    case None => (letter, count)
+  )
+
+
+/**
+ * @param word String, like "in"
+ * @param hand Hand, like Map('e' -> 2, 'i' -> 1, 'n' -> 3, 'c' -> 1)
+ * @param wordList List of ALL UPPERCASE Strings, a dictionary to look up word
+ * @return true if word consists entirely of letters in hand, and is in wordList
+ */
+def isValidWord(word: String, hand: Hand, wordList: Words): Boolean =
+  val wordHand: Hand = getFreqMap(word)
+  val wordIsInHand: Boolean = word.forall(char =>
+    hand.contains(char) && wordHand(char) <= hand(char)
+  )
+  wordList.contains(word.toUpperCase) && wordIsInHand
+
+
+/**
+ * @param hand a Map of char, int pairs. Hand of letters to play.
+ * @param words the dictionary to look up valid words.
+ * @param num hand size. Needed to calculate score.
+ *            Plays one hand of the word guessing game.
+ *            Uses var (mutation), breakable while loops and break keyword.
+ */
+def playHand(hand: Hand, words: Words, num: Int): Unit =
+  var score: Int = 0
+  var currentHand: Hand = hand
+
+  breakable(
+    while
+      currentHand.values.sum > 0
+    do
+      displayHand(currentHand)
+      val word: String = readLine("Enter a word, or a . to indicate you are finished: ")
+      if word == "." then
+        break
+      else if !isValidWord(word, hand, words) then
+        println("Invalid word, please try again.")
+      else
+        val wordScore: Int = getWordScore(word, num)
+        score += wordScore
+        println(f"$word earned $wordScore points. Total: $score points.")
+        currentHand = updateHand(currentHand, word)
+  )
+  if currentHand.values.sum == 0 then
+    println(f"Ran out of letters. Total score: $score points.")
+  else
+    println(f"Goodbye! Total score: $score points.")
+
+
+/**
+ * @param words list of strings, a dictionary to look up words.
+ * @param handSize integer, the number of letters in a hand
+ * @param vowelRatio divide handSize by this to find how many should be vowels.
+ *                   uses var (mutation) and nested while loops.
+ */
+def playGame(words: Words, handSize: Int, vowelRatio: Int): Unit =
+  var lastHand: Hand = Map()
+  var command: String = ""
+
+  while
+    true
+  do
+    command = readLine("Enter n to deal a new hand, r to replay last hand, or e to end the game: ")
+    while
+      command == "r" && lastHand.isEmpty
+    do
+      println("You have not played a hand yet. Please play a new hand first!")
+      command = readLine("Enter n to deal a new hand, r to replay last hand, or e to end the game: ")
+    if command == "n" then
+      val newHand = dealHand(handSize, handSize / vowelRatio)
+      lastHand = newHand
+      playHand(newHand, words, handSize)
+    if command == "r" then
+      playHand(lastHand, words, handSize)
+    if command == "e" then
+      return
+    if !List("e", "n", "r").contains(command) then
+      println("Invalid command.")
+
+
+@main def hello: Unit =
+  val WORDS: Words = loadWords(FILENAME)
+  playGame(WORDS, HANDSIZE, VOWELRATIO)
+  println("I was compiled with Scala 3 :)")
---
+++ src/test/scala/Test1.scala
@@ -0,0 +1,92 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+val wordList: Words = loadWords(FILENAME)
+
+class WordScore: // name of the class does not matter as long as it's not "Test"
+  @Test def `testing getWordScore`(): Unit =
+    val wordScores: List[(String, Int)] = List(
+      ("", 0),
+      ("it", 4),
+      ("was", 18),
+      ("fork", 44),
+      ("scored", 54),
+      ("outgnaw", 127),
+      ("waybill", 155)
+    )
+    for
+      (word, score) <- wordScores
+    do
+      assertEquals(getWordScore(word, 7), score)
+
+class UpdateHand:
+  @Test def `testing updateHand: 1`(): Unit =
+    val hand: Hand = Map(
+      'a' -> 1, 'q' -> 1, 'l' -> 2, 'm' -> 1, 'u' -> 1, 'i' -> 1
+    )
+    val word: String = "quail"
+    val expected: Hand = Map(
+      'a' -> 0, 'q' -> 0, 'l' -> 1, 'm' -> 1, 'u' -> 0, 'i' -> 0
+    )
+    assertEquals(updateHand(hand, word), expected)
+
+  @Test def `testing updateHand: 2`(): Unit =
+    val hand: Hand = Map('e' -> 1, 'v' -> 2, 'n' -> 1, 'l' -> 2, 'i' -> 1)
+    val word: String = "evil"
+    val expected: Hand = Map('e' -> 0, 'v' -> 1, 'n' -> 1, 'l' -> 1, 'i' -> 0)
+    assertEquals(updateHand(hand, word), expected)
+
+  @Test def `testing updateHand: 3`(): Unit =
+    val hand: Hand = Map('e' -> 1, 'h' -> 1, 'o' -> 1, 'l' -> 2)
+    val word: String = "hello"
+    val expected: Hand = Map('e' -> 0, 'h' -> 0, 'o' -> 0, 'l' -> 0)
+    assertEquals(updateHand(hand, word), expected)
+
+class IsValidWord:
+  @Test def `testing isValidWord: 1`(): Unit =
+    val word1: String = "hello"
+    val hand1: Hand = Map('h' -> 1, 'e' -> 1, 'l' -> 2, 'o' -> 1)
+    assertEquals(isValidWord(word1, hand1, wordList), true)
+
+  @Test def `testing isValidWord: 2`(): Unit =
+    val word2: String = "hello"
+    val hand2: Hand = Map('h' -> 1, 'e' -> 1, 'l' -> 1, 'o' -> 1)
+    assertEquals(isValidWord(word2, hand2, wordList), false)
+
+  @Test def `testing isValidWord: 3`(): Unit =
+    val word3: String = "rapture"
+    val hand3: Hand = Map(
+      'r' -> 1, 'a' -> 3, 'p' -> 2, 't' -> 1, 'u' -> 1, 'e' -> 1
+    )
+    assertEquals(isValidWord(word3, hand3, wordList), false)
+
+  @Test def `testing isValidWord: 4`(): Unit =
+    val word4: String = "rapture"
+    val hand4: Hand = Map(
+      'r' -> 2, 'a' -> 1, 'p' -> 1, 't' -> 1, 'u' -> 1, 'e' -> 1
+    )
+    assertEquals(isValidWord(word4, hand4, wordList), true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
